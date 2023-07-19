082d25065a18bc81dd7000cb532897813d7ec13f
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-07-19 20:10:53 +0530

    finished logic

--- Asset/script/index.js
+++ Asset/script/index.js
@@ -1,15 +1,48 @@
 
-//The onload executes a block of code after the page is completely loaded 
-
-window.onload()=function(){
-
     var seconds=0;
     var tens=0;
     var appendSeconds=document.getElementById('seconds');
     var appendtens=document.getElementById('tens');
-    var btnStart=document.getElementById('start-btn');
+    let btnStart=document.getElementById('start-btn');
     var btnReset=document.getElementById('reset-btn');
     var btnStop=document.getElementById('stop-btn');
     var interval;
 
-}
+    btnStart.onclick=function(){
+        clearInterval(interval);
+        interval=setInterval(startTimer,10);
+    }
+
+    btnReset.onclick=function(){
+        clearInterval(interval);
+        tens="00";
+        seconds="00";
+        appendSeconds.innerHTML=seconds;
+        appendtens.innerHTML=tens;
+    }
+
+    btnStop.onclick=function(){
+        clearInterval(interval);
+    }
+
+    const startTimer=function(){
+
+        tens++
+
+        if(tens<=9){
+            appendtens.innerHTML="0"+tens;
+        }else{
+            appendtens.innerHTML=tens;
+        }
+
+        if(tens>99){
+            seconds++
+            appendSeconds.innerHTML="0"+seconds;
+            tens=0;
+            appendtens.innerHTML="0"+0;
+        }
+
+        if(seconds>9){
+            appendSeconds.innerHTML=seconds;
+        }
+    }
