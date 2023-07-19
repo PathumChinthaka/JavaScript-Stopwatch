bf3e6b5924c1e3b1f20ce7644c54c83ba357392b
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-07-19 19:24:06 +0530

    variables

---
+++ Asset/script/index.js
@@ -0,0 +1,15 @@
+
+//The onload executes a block of code after the page is completely loaded 
+
+window.onload()=function(){
+
+    var seconds=0;
+    var tens=0;
+    var appendSeconds=document.getElementById('seconds');
+    var appendtens=document.getElementById('tens');
+    var btnStart=document.getElementById('start-btn');
+    var btnReset=document.getElementById('reset-btn');
+    var btnStop=document.getElementById('stop-btn');
+    var interval;
+
+}
